4a2bb27f1815e39f8a600f3ab4dc841069198ce6
Andrei <andreibuzas@gmail.com>
2020-05-21 21:31:14 +0300

    added Car class

---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,15 @@
+package org.fasttrackit;
+
+public class Car {
+
+    String make;
+    String model;
+    String color;
+    double mileage;
+    double fuelLevel;
+    double maxSpeed;
+    double totalTravelledDistance;
+    int doorCount;
+    boolean damaged;
+    
+}
