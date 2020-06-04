4b244ace5c858adc6046c3a752bc2ec8b8305870
Andrei <andreibuzas@gmail.com>
2020-06-04 21:11:03 +0300

    studied if else statements

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -12,6 +12,23 @@ public class Vehicle {
     boolean damaged;
 
     public double accelerate(double speed, double durationInHours) {
+        if(speed > maxSpeed) {
+            System.out.println("MaxSpeed exceeded.");
+            return 0;
+        }
+        else if(speed == maxSpeed) {
+            System.out.println("Careful MaxSpeed reached.");
+        }
+        else {
+            System.out.println("Valid speed entered.");
+        }
+
+        if(damaged) {
+            System.out.println("The vehicle is damaged. You cannot accelerate.");
+            return 0;
+        }
+
+
         System.out.println(make + " is accelerating with " + speed + " km/h for " + durationInHours + "h.");
 
         double distance = speed * durationInHours;
